6f85e3765334d2c230c6c75e2d81badf85c320d4
paulbrause <paulbrause@googlemail.com>
2024-04-10 16:04:15 +0200

    Lesson 5

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-a = input()
-b = input()
+print("Welcome to the Band Name Generator")
 
-c = b
-b = a
-a = c
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
 
-print("a: " + a)
-print("b: " + b)
+print("Your band name could be " + city + " " + pet)
